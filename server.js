f4990db1aac390eb40dfff3043afeca80026e875
panachai <panachai@treg.co.th>
2022-07-12 14:41:44 +0700

    add start record (not work)

--- server.js
+++ server.js
@@ -13,14 +13,11 @@ const uid = Math.floor(Math.random() * 100000);
 const customerId = process.env.AGORA_CUSTOMER_ID;
 const customerSecret = process.env.AGORA_CUSTOMER_SECRET;
 
-const authToken = Buffer.from(`${customerId}:${customerSecret}`).toString(
-  "base64"
-);
+const Authorization =
+  "Basic " + Buffer.from(`${customerId}:${customerSecret}`).toString("base64");
 
 // step 1
 app.post("/acquire", async (req, res) => {
-  const Authorization = `Basic ${authToken}`;
-
   try {
     const acquire = await axios.post(
       `https://api.agora.io/v1/apps/${appID}/cloud_recording/acquire`,
@@ -45,8 +42,58 @@ app.post("/acquire", async (req, res) => {
 });
 
 // step 2
-app.post("/start", (req, res) => {
-  // Start Cloud Recording Here
+app.post("/start", async (req, res) => {
+  const resource = req.body.resource;
+  const mode = req.body.mode;
+
+  const appID = process.env.AGORA_APP_ID;
+  const bucket = process.env.S3_BUCKET;
+  const accessKey = process.env.S3_ACCESS_KEY;
+  const secretKey = process.env.S3_SECRET_KEY;
+  const region = process.env.S3_REGION;
+  const vendor = process.env.S3_VENDOR;
+
+  try {
+    const start = await axios.post(
+      `https://api.agora.io/v1/apps/${appID}/cloud_recording/resourceid/${resource}/mode/${mode}/start`,
+      {
+        cname: req.body.channel,
+        uid: req.body.uid,
+        clientRequest: {
+          recordingConfig: {
+            maxIdleTime: 30,
+            // Both audio and video streams.
+            streamTypes: 2,
+            channelType: 0,
+            videoStreamType: 0,
+            transcodingConfig: {
+              height: 640,
+              width: 360,
+              bitrate: 500,
+              fps: 15,
+              mixedVideoLayout: 1,
+              backgroundColor: "#FFFFFF",
+            },
+          },
+          recordingFileConfig: {
+            avFileType: ["hls"],
+          },
+          storageConfig: {
+            vendor: vendor,
+            region: region,
+            bucket: bucket,
+            accessKey: accessKey,
+            secretKey: secretKey,
+            fileNamePrefix: ["videos"],
+          },
+        },
+      },
+      { headers: { Authorization } }
+    );
+    res.status(200).send(body);
+  } catch (e) {
+    res.status(e.response.status).send(e);
+  }
 });
 
 app.get("/", (req, res) => res.send("Agora Cloud Recording Server"));
